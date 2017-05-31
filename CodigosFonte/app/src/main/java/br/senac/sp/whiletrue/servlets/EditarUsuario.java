74d0e6b649589b7c89bd555b442e8fa51b4c092e
Gabriel Malta <gmalta44@gmail.com>
2017-05-31 20:50:42 -0300

    ajustes no servlet de usuario

--- CodigosFonte/app/src/main/java/br/senac/sp/whiletrue/servlets/EditarUsuario.java
+++ CodigosFonte/app/src/main/java/br/senac/sp/whiletrue/servlets/EditarUsuario.java
@@ -5,7 +5,6 @@ import br.senac.sp.whiletrue.servico.FilialService;
 import br.senac.sp.whiletrue.servico.PerfilService;
 import br.senac.sp.whiletrue.servico.UsuarioService;
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -56,23 +55,27 @@ public class EditarUsuario extends HttpServlet {
             String senha = request.getParameter("senha");
             Usuario usuario = new Usuario(id, 0, 0, nome, email, null, false, null);
             usuario.setSenha(senha);
+            
+            usuario.setHashSenha(senha);
+            service.salvar(usuario);
+            response.sendRedirect(request.getContextPath() + "/usuarios");
 
-            ArrayList<String> erros = service.validar(usuario);
-            if (erros.isEmpty() == true) {
-                usuario.setHashSenha(senha);
-                service.salvar(usuario);
-                response.sendRedirect(request.getContextPath() + "/usuarios");
-            } else {
-                perfilService = new PerfilService();
-                filialService = new FilialService();
-
-                request.setAttribute("listaPerfil", perfilService.listar());
-                request.setAttribute("listaFilial", filialService.listar());
-                request.setAttribute("usuariotoedit", usuario);
-
-                request.getRequestDispatcher("/WEB-INF/usuario/editar.jsp")
-                        .forward(request, response);
-            }
+//            ArrayList<String> erros = service.validar(usuario);
+//            if (erros.isEmpty() == true) {
+//                usuario.setHashSenha(senha);
+//                service.salvar(usuario);
+//                response.sendRedirect(request.getContextPath() + "/usuarios");
+//            } else {
+//                perfilService = new PerfilService();
+//                filialService = new FilialService();
+//
+//                request.setAttribute("listaPerfil", perfilService.listar());
+//                request.setAttribute("listaFilial", filialService.listar());
+//                request.setAttribute("usuariotoedit", usuario);
+//
+//                request.getRequestDispatcher("/WEB-INF/usuario/editar.jsp")
+//                        .forward(request, response);
+//            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
